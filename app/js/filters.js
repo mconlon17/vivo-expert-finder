b2e66505ebb0508b8c4aa85522f292b94177f33c
Michael Conlon <mconlon@ufl.edu>
2014-09-06 11:40:56 -0400

    Add a filter for displaying counts using singular or plural unit(s)

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,18 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
+angular.module('myApp.filters', [])
+  .filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     };
-  }]);
+  }])
+
+  // Usage:  {{number|unit:phone}} will produce 2 phones or 1 phone
+  .filter('unit', function() {
+    return function(number, label) {
+      if (number == 1) { return(number.toString() + ' ' + label); }
+      else {return(number.toString() + ' ' + label + 's'); }
+  };})
+
+  ;
